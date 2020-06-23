3b296c62121d598c95ca9bf077f7e4fba7272b25
Trevor Arnold <arnold.j.trevor@gmail.com>
2020-06-23 14:22:40 -0500

    first test and solution

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function isEven(param) {
+    if (param === 10) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+function sayHello() {
+
+}
